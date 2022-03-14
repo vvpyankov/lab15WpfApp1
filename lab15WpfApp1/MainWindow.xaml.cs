30ef7908ce054ce084ddf5660dfed983173a7219
vvpyankov@mail.ru <vvpyankov@mail.ru>
2022-03-15 00:23:08 +0300

    Добавьте файлы проекта.

---
+++ lab15WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Xps;
+using System.Windows.Xps.Packaging;
+
+namespace lab15WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            XpsDocument doc = new XpsDocument("1.xps", FileAccess.Write);
+            XpsDocumentWriter writer = XpsDocument.CreateXpsDocumentWriter(doc);
+            writer.Write(documentViewer.Document as FixedDocument);
+            doc.Close();
+        }
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            XpsDocument doc = new XpsDocument("D:/ITMO/projects/lab15WpfApp1/lab15WpfApp1/Data/FixedDocument.xps", FileAccess.Read);
+            documentViewer.Document = doc.GetFixedDocumentSequence();
+            doc.Close();
+        }
+    }
+}
---
+++ lab15flowDocWpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xaml;
+
+
+namespace lab15flowDocWpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /* private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            docViewer.ClearValue(FlowDocumentScrollViewer.DocumentProperty);
+        }
+
+       private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Open("1.xaml", FileMode.Create))
+            {
+                XamlWriter.Save(docViewer.Document, fs);
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Open("1.xaml", FileMode.Open))
+            {
+                docViewer.Document = XamlReader.Load(fs) as FlowDocument;
+            }
+        }*/
+    }
+}
